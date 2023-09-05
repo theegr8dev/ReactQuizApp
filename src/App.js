9215df0d9fd8d6c81cdcb011a3a16c03bba3b7a5
Alabi Khalid <alabikhalid.12@gmail.com>
2023-09-05 22:06:59 +0100

    loading question from fake api upadated the package.json with a script to start api with json-server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+import { useEffect, useReducer } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
+const initialState = {
+	questions: [],
+	// 'loading','error','ready','active','finised'(type of status)
+	status: 'loading',
+};
+function reducer(state, action) {
+	switch (action.type) {
+		case 'dataReceived':
+			return { ...state, questions: action.payload, status: 'ready' };
+		case 'dataFailed':
+			return { ...state, status: 'error' };
+		default:
+			throw new Error('Action unKnown');
+	}
+}
 export default function App() {
+	const [state, dispatch] = useReducer(reducer, initialState);
+	useEffect(() => {
+		fetch('http://localhost:8000/questions')
+			.then(res => res.json())
+			.then(data => dispatch({ type: 'dataReceived', payload: data }))
+			.catch(err => dispatch({ type: 'dataFailed' }));
+	}, []);
 	return (
 		<div className="app">
 			<Header />
